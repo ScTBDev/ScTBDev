8b0fb84e5a30ad92d2de1f4007f518d1a54e2b58
djGrrr <github@linuxhosted.ca>
2010-05-04 16:48:51 +0000

    experiment with a new way of doing define_bits

--- public_up_html/include/define_bits.php
+++ public_up_html/include/define_bits.php
@@ -18,76 +18,19 @@
  *	You should have received a copy of the GNU General Public License
  *	along with ScTBDev.  If not, see <http://www.gnu.org/licenses/>.
  */
-require_once(__DIR__.DIRECTORY_SEPARATOR.'defines.php');
 
-const BIT_1		= 0x1;
-const BIT_2		= 0x2;
-const BIT_3		= 0x4;
-const BIT_4		= 0x8;
-const BIT_5		= 0x10;
-const BIT_6		= 0x20;
-const BIT_7		= 0x40;
-const BIT_8		= 0x80;
-const BIT_9		= 0x100;
-const BIT_10	= 0x200;
-const BIT_11	= 0x400;
-const BIT_12	= 0x800;
-const BIT_13	= 0x1000;
-const BIT_14	= 0x2000;
-const BIT_15	= 0x4000;
-const BIT_16	= 0x8000;
-const BIT_17	= 0x10000;
-const BIT_18	= 0x20000;
-const BIT_19	= 0x40000;
-const BIT_20	= 0x80000;
-const BIT_21	= 0x100000;
-const BIT_22	= 0x200000;
-const BIT_23	= 0x400000;
-const BIT_24	= 0x800000;
-const BIT_25	= 0x1000000;
-const BIT_26	= 0x2000000;
-const BIT_27	= 0x4000000;
-const BIT_28	= 0x8000000;
-const BIT_29	= 0x10000000;
-const BIT_30	= 0x20000000;
-const BIT_31	= 0x40000000;
-// can only define BIT_32 like this if you have > 32 bit cpu/os/etc
-const BIT_32	= 0x80000000;
+$apc = PHP_SAPI === 'cli' ? false : extension_loaded('apc');
+$loaded = $apc ? apc_load_constants('BITS', true) : false;
+if (!$loaded) {
+	$bits = array();
+	for ($i = 0, $num = PHP_INT_SIZE * 8; $i < $num; $i++)
+		$bits['BIT_'.($i + 1)] = 1 << $i;
 
-const BIT_33	= 0x100000000;
-const BIT_34	= 0x200000000;
-const BIT_35	= 0x400000000;
-const BIT_36	= 0x800000000;
-const BIT_37	= 0x1000000000;
-const BIT_38	= 0x2000000000;
-const BIT_39	= 0x4000000000;
-const BIT_40	= 0x8000000000;
-const BIT_41	= 0x10000000000;
-const BIT_42	= 0x20000000000;
-const BIT_43	= 0x40000000000;
-const BIT_44	= 0x80000000000;
-const BIT_45	= 0x100000000000;
-const BIT_46	= 0x200000000000;
-const BIT_47	= 0x400000000000;
-const BIT_48	= 0x800000000000;
-const BIT_49	= 0x1000000000000;
-const BIT_50	= 0x2000000000000;
-const BIT_51	= 0x4000000000000;
-const BIT_52	= 0x8000000000000;
-const BIT_53	= 0x10000000000000;
-const BIT_54	= 0x20000000000000;
-const BIT_55	= 0x40000000000000;
-const BIT_56	= 0x80000000000000;
-const BIT_57	= 0x100000000000000;
-const BIT_58	= 0x200000000000000;
-const BIT_59	= 0x400000000000000;
-const BIT_60	= 0x800000000000000;
-const BIT_61	= 0x1000000000000000;
-const BIT_62	= 0x2000000000000000;
-const BIT_63	= 0x4000000000000000;
-// can only define BIT_64 like this if you have > 64 bit cpu/os/etc
-//const BIT_64	= 0x8000000000000000;
-
-//define('BIT_32', 1 << 31);	// if on 32 bit CPU, define BIT_32 like this
-define('BIT_64', 1 << 63);		// if on 64 bit CPU, define BIT_64 like this
+	if ($apc)
+		apc_define_constants('BITS', $bits, true);
+	else {
+		foreach ($bits as $key => $value)
+			define($key, $value);
+	}
+}
 ?>
