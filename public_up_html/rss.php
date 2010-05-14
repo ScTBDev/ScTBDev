f24adb1081b35e8b1164c3d6e7715538ae6ebee3
djGrrr <github@linuxhosted.ca>
2010-05-14 16:34:27 +0000

    adding rss feeds

---
+++ public_up_html/rss.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ *	ScTBDev - A bittorrent tracker source based on SceneTorrents.org
+ *	Copyright (C) 2005-2010 ScTBDev.ca
+ *
+ *	This file is part of ScTBDev.
+ *
+ *	ScTBDev is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	ScTBDev is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with ScTBDev.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once(__DIR__.DIRECTORY_SEPARATOR.'include'.DIRECTORY_SEPARATOR.'defines.php');
+require_once(CLASS_PATH.'bt_memcache.php');
+require_once(CLASS_PATH.'bt_mem_caching.php');
+require_once(CLASS_PATH.'bt_security.php');
+require_once(CLASS_PATH.'bt_sql.php');
+require_once(CLASS_PATH.'bt_vars.php');
+
+$num = isset($_GET['num']) ? (0 + $_GET['num']) : 10;
+$strip = isset($_GET['strip']) ? 1 : 0;
+$stripun = isset($_GET['stripun']) ? 1 : 0;
+
+if ($num > 50)
+	$num = 50;
+elseif ($num < 5)
+	$num = 5;
+
+$passkey = isset($_GET['passkey']) ? $_GET['passkey'] : '';
+if (!$passkey)
+	die;
+
+$user = bt_mem_caching::get_user_from_passkey($passkey);
+if (!$user)
+	die;
+
+bt_memcache::connect();
+header('Content-Type: application/xml');
+echo '<?xml version="1.0" encoding="iso-8859-1" ?>'."\n".'<rss version="2.0">'."\n\t".'<channel>'."\n\t\t".'<title>ScT</title>'."\n\t\t".
+	'<description>ScT RSS Feed</description>'."\n\t\t".'<link>'.bt_vars::$base_url.'/</link>'."\n\t\t".'<language>en-US</language>'."\n";
+
+$last_torrent = bt_memcache::get('last_torrent');
+if (!$last_torrent) {
+	bt_sql::connect();
+
+	$ltorrentq = bt_sql::query('SELECT id FROM torrents ORDER BY id DESC LIMIT 1') or bt_sql::err(__FILE__, __LINE__);
+	$lt = $ltorrentq->fetch_row();
+	$ltorrentq->free();
+	$last_torrent = 0 + $lt[0];
+	bt_memcache::add('last_torrent', $last_torrent, 10800);
+}
+
+$whereq = array();
+if (isset($_GET['c']) && is_array($_GET['c'])) {
+	$cats = array();
+	foreach ($_GET['c'] as $cat)
+		$cats[] = (int)$cat;
+
+	$cats = array_unique($cats);
+	sort($cats, SORT_NUMERIC);
+	$whereq[] = 'category IN ('.implode(', ', $cats).')';
+}
+
+$where = count($whereq) ? ' WHERE '.implode(' AND ', $whereq) : '';
+
+$key = 'rss::norm:::'.sha1($where);
+$torrents = bt_memcache::get($key);
+
+if ($torrents === false || $torrents['last'] != $last_torrent) {
+	$torrents = array();
+	$torrents['last'] = $last_torrent;
+	$torrents['list'] = array();
+
+	bt_sql::connect();
+
+	$torrentsq = bt_sql::query('SELECT t.id, t.name, t.descr, t.added, c.name AS catname FROM torrents AS t '.
+		'LEFT JOIN categories AS c ON (c.id = t.category)'.$where.' ORDER BY t.id DESC LIMIT 50') or bt_sql::err(__FILE__, __LINE__);
+
+	while ($torrent = $torrentsq->fetch_assoc()) {
+		$torrent['id'] = 0 + $torrent['id'];
+		$torrent['added'] = 0 + $torrent['added'];
+		$torrent['name_strip'] = bt_security::html_safe(strtr($torrent['name'], '._', '  '), true);
+		$torrent['name_stripun'] = bt_security::html_safe(strtr($torrent['name'], '_', ' '), true);
+		$torrent['name'] = bt_security::html_safe($torrent['name'], true);
+		$torrent['catname'] = $torrent['catname'] ? bt_security::html_safe($torrent['catname'], true) : 'None';
+		$torrent['descr'] = bt_security::html_safe($torrent['descr'], true);
+		$torrents['list'][] = $torrent;
+	}
+	$torrentsq->free();
+
+	bt_memcache::set($key, $torrents, 10800, true);
+}
+
+
+for ($i = 0; $i < $num; $i++) {
+	if (!isset($torrents['list'][$i]))
+		break;
+
+	$row = $torrents['list'][$i];
+	if ($strip)
+		$title = $row['name_strip'];
+	elseif ($stripun)
+		$title = $row['name_stripun'];
+	else
+		$title = $row['name'];
+	
+	echo "\t\t".'<item>'."\n\t\t\t".'<title>'.$title.'</title>'."\n\t\t\t".'<pubDate>'.gmdate('D, d M Y H:i:s', $row['added']).
+		' +0000</pubDate>'."\n\t\t\t".'<category>'.$row['catname'].'</category>'."\n\t\t\t".'<link>'.bt_vars::$base_url.
+		'/details.php?id='.$row['id'].'&amp;hit=1</link>'."\n\t\t\t".'<description>'.$row['descr'].'</description>'."\n\t\t".
+		'</item>'."\n";
+}
+
+echo "\t".'</channel>'."\n".'</rss>';
+?>
---
+++ public_up_html/rsser.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ *	ScTBDev - A bittorrent tracker source based on SceneTorrents.org
+ *	Copyright (C) 2005-2010 ScTBDev.ca
+ *
+ *	This file is part of ScTBDev.
+ *
+ *	ScTBDev is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	ScTBDev is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with ScTBDev.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once(__DIR__.DIRECTORY_SEPARATOR.'include'.DIRECTORY_SEPARATOR.'bittorrent.php');
+
+dbconn();
+loggedinorreturn();
+
+bt_theme::head('RSS Feed Configurator');
+$tsettings = bt_theme::$settings['rsser'];
+$radio_on = $tsettings['radio_on'];
+$check_on = $tsettings['check_on'];
+
+
+$cats = bt_mem_caching::get_cat_list();
+
+$urls = array();
+$urls[] = 'passkey='.bt_user::$current['passkey'];
+
+$selcats = array();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if (isset($_POST['num']))
+		$num = (int)$_POST['num'];
+		
+
+	if (isset($_POST['c']) && is_array($_POST['c'])) {
+		foreach ($_POST['c'] as $c)
+			$selcats[((int)$c)] = true;
+	}
+}
+else {
+	foreach ($cats as $cat) {
+		if (strpos(bt_user::$current['notifs'], '[cat'.$cat['id'].']') !== false)
+			$selcats[$cat['id']] = true;
+	}
+	reset($cats);
+}
+$num = $num ? ($num < 5 ? 5 : ($num > 50 ? 50 : $num)) : 10;
+if ($num != 10)
+	$urls[] = 'num='.$num;
+$feed = $_SERVER['REQUEST_METHOD'] == 'POST' ? (int)$_POST['feed'] : 1;
+$feed_1 = $feed != 2 ? $radio_on : '';
+$feed_2 = $feed == 2 ? $radio_on : '';
+
+$ssl = $_SERVER['REQUEST_METHOD'] == 'POST' ? ((bool)0 + $_POST['ssl']) : (bt_user::$current['settings']['ssl_site'] || bt_vars::$ssl);
+$ssl_on = $ssl ? $check_on : '';
+
+$catrows = $catentrys = array();
+$ncats = count($cats);
+$i = 0;
+
+foreach ($cats as $cat) {
+	if (isset($selcats[$cat['id']])) {
+		$checked = $check_on;
+		$urls[] = 'c[]='.$cat['id'];
+	}
+	else
+		$checked = '';
+	$catentrys[] = sprintf($tsettings['entry'], $cat['id'], $checked,  $cat['ename']);
+	$i++;
+	$catsleft = $i % $tsettings['per_row'];
+	if ($catsleft == 0 || $i == $ncats) {
+		$catrows[] = implode($tsettings['join'], $catentrys);
+		$catentrys = array();
+	}
+}
+$cat_table = implode($tsettings['join_row'], $catrows);
+
+$feed_url = ($ssl ? bt_config::$conf['default_ssl_url'] : bt_config::$conf['default_plain_url']).'/rss'.($feed == 2 ? '' : 'dl').'.php?'.implode('&amp;', $urls);
+
+$rsservars = array(
+	'FEED_URL'	=> $feed_url,
+	'CAT_TABLE'	=> $cat_table,
+	'FEED_1'	=> $feed_1,
+	'FEED_2'	=> $feed_2,
+	'SSL_ON'	=> $ssl_on,
+	'NUM'		=> $num,
+);
+
+echo bt_theme_engine::load_tpl('rsser', $rsservars);
+
+bt_theme::foot();
+?>
